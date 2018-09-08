76299bedcc40e152facf1255a8d5c3ac4d1867b8
Sergio Monroy Escalante <sergio.monroy.escalante@gmail.com>
2018-09-08 17:56:14 -0400

    Initial Commit

---
+++ Scripts/game.js
@@ -0,0 +1,11 @@
+// IIFE - Immediately Invoked Function Expression
+(function(){
+    function Start() {
+        console.log(`%c Game Started`, "color:blue; font-size: 20px;");
+    }
+    function Update() {
+
+    }
+
+    window.addEventListener("load", Start);
+})();
